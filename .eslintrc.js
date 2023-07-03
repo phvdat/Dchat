25167131a628029eca85d3867587585f83d918e0
phvdat <phamvandat.hcmut@gmail.com>
2023-07-03 18:20:02 +0700

    WIP: sidebar

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,56 +1,56 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
-  },
-  extends: ['eslint:recommended', 'prettier'],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: ['.eslintrc.{js,jsx,ts,tsx}'],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  plugins: ['@typescript-eslint', 'react', 'prettier', 'simple-import-sort'],
-  rules: {
-    'no-console': 'warn',
-    'no-eval': 'error',
-    'no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        args: 'after-used'
-      }
-    ],
-    semi: ['error', 'always'],
-    'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off',
-    quotes: [2, 'single'],
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-        printWidth: 80,
-        trailingComma: 'none',
-        singleQuote: true,
-        jsxSingleQuote: true
-      }
-    ],
+	env: {
+		browser: true,
+		es2021: true
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	overrides: [
+		{
+			env: {
+				node: true
+			},
+			files: ['.eslintrc.{js,jsx,ts,tsx}'],
+			parserOptions: {
+				sourceType: 'script'
+			}
+		}
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module'
+	},
+	plugins: ['@typescript-eslint', 'eslint', 'react', 'prettier', 'simple-import-sort'],
+	rules: {
+		'no-console': 'warn',
+		'no-eval': 'error',
+		'no-unused-vars': [
+			'warn',
+			{
+				vars: 'all',
+				args: 'after-used'
+			}
+		],
+		semi: ['error', 'always'],
+		'react/react-in-jsx-scope': 'off',
+		'react/prop-types': 'off',
+		quotes: [2, 'single'],
+		'prettier/prettier': [
+			'error',
+			{
+				endOfLine: 'auto',
+				printWidth: 80,
+				trailingComma: 'none',
+				singleQuote: true,
+				jsxSingleQuote: true
+			}
+		],
 
-    'simple-import-sort/imports': [
-      'error',
-      {
-        groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']]
-      }
-    ]
-  }
+		'simple-import-sort/imports': [
+			'error',
+			{
+				groups: [['^\\u0000', '^@?\\w', '^[^.]', '^\\.']]
+			}
+		]
+	}
 };
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,37 +1,62 @@
 import Icon from 'components/baseUI/Icon';
+import { useLayoutEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  const darkModeLocal = localStorage.getItem('color-theme');
+
   const handleSwitchMode = () => {
-    if (
-      localStorage.getItem('color-theme') &&
-      localStorage.getItem('color-theme') === 'light'
-    ) {
-      document.documentElement.classList.add('dark');
-      localStorage.setItem('color-theme', 'dark');
-    } else {
+    if (darkMode) {
       document.documentElement.classList.remove('dark');
       localStorage.setItem('color-theme', 'light');
+      setDarkMode(false);
+    } else {
+      document.documentElement.classList.add('dark');
+      localStorage.setItem('color-theme', 'dark');
+      setDarkMode(true);
     }
   };
 
+  useLayoutEffect(() => {
+    if (darkModeLocal && darkModeLocal === 'light') {
+      setDarkMode(false);
+    } else {
+      setDarkMode(true);
+    }
+  }, [darkModeLocal]);
+
   return (
-    <div className='w-10'>
-      <button
-        onClick={handleSwitchMode}
-        id='theme-toggle'
-        type='button'
-        className='bg-gray-200 dark:bg-gray-700 rounded-md p-2 border-2'
-      >
-        dark mode
-      </button>
-      <div>CHATD</div>
-      <div className='flex flex-col gap-3'>
-        <Icon icon='user-solid' />
-        <Icon icon='message' />
-        <Icon icon='group' />
-        <Icon icon='setting' />
+    <div className='w-fit flex flex-col justify-between items-center py-6 bg-tertiary-light dark:bg-tertiary-dark'>
+      <NavLink to='#' className='px-1'>
+        <h1 className='font-bold'>CHATD</h1>
+      </NavLink>
+      <div className='flex flex-col gap-1'>
+        <NavLink to='/' className='py-2  px-4 rounded-md'>
+          <Icon icon='user-solid' size={20} />
+        </NavLink>
+        <NavLink to='#' className='py-2  px-4'>
+          <Icon icon='message' size={20} />
+        </NavLink>
+        <NavLink to='#' className='py-2  px-4'>
+          <Icon icon='user-group-solid' size={20} />
+        </NavLink>
+        <NavLink to='#' className='py-2  px-4'>
+          <Icon icon='setting' size={20} />
+        </NavLink>
       </div>
-      <div></div>
+      <div className='flex flex-col gap-3 py-2 w-full'>
+        <div className='w-full text-center py-2'>
+          <Icon icon='globe-solid' size={20} className='cursor-pointer' />
+        </div>
+        <div onClick={handleSwitchMode} className='w-full text-center py-2'>
+          <Icon
+            icon={darkMode ? 'moon-solid' : 'sun'}
+            size={20}
+            className='cursor-pointer'
+          />
+        </div>
+      </div>
     </div>
   );
 };
